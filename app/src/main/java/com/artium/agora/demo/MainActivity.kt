55608fe73f35bb0920e4374e4a7d59d3e4d7c1a7
Suryavel Tamilselvan Rani <suryavel_tr@Suryavels-MacBook-Pro.local>
2022-03-24 17:21:11 +0530

    Done one-to-one with video SDK.

--- app/src/main/java/com/artium/agora/demo/MainActivity.kt
+++ app/src/main/java/com/artium/agora/demo/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import io.agora.rtc.IRtcEngineEventHandler
@@ -20,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     // Fill the temp token generated on Agora Console.
     private val token =
-        "0067eae3982dde04b9a8e986109c307682cIAC1N07bM5Z21Mk5A512H9xQgwd6I8O0U8qZ+lEZ4bM/6REfIGgAAAAAEABHuwtvEzs8YgEAAQATOzxi"
+        "006f2b6d3398e0d42418815bee3b7974e5fIADuStcCHC5IcxOVSP1unCL63oFGjtUdqiXpjOrobLTI0BEfIGgAAAAAEAAhqfJE76U9YgEAAQDspT1i"
 
     // Fill the channel name.
     private val channelName = "artium_demo_ch"
@@ -30,8 +29,6 @@ class MainActivity : AppCompatActivity() {
     private val requestPermissionCallbacks = mutableListOf<ActivityResultLauncher<String>>()
 
     private companion object {
-        private const val PERMISSION_REQ_ID_RECORD_AUDIO = 22
-        private const val PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1
         private const val APP_PERMISSION_REQUEST = 5500
     }
 
@@ -42,7 +39,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onUserJoined(uid: Int, elapsed: Int) {
-            super.onUserJoined(uid, elapsed)
+            runOnUiThread {
+                setUpRemoteUserVideoView(uid)
+            }
+        }
+
+        override fun onUserOffline(uid: Int, reason: Int) {
+            super.onUserOffline(uid, reason)
+        }
+
+        override fun onUserMuteVideo(uid: Int, muted: Boolean) {
+            super.onUserMuteVideo(uid, muted)
+        }
+
+        override fun onUserMuteAudio(uid: Int, muted: Boolean) {
+            super.onUserMuteAudio(uid, muted)
         }
 
         override fun onLeaveChannel(stats: RtcStats?) {
@@ -54,16 +65,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        getPermissionResultCallback {
-//            getPermissionResultCallback().let {
-//                requestPermissionCallbacks.add(it)
-//                requestPermission(it, Manifest.permission.RECORD_AUDIO)
-//            }
-//        }.let {
-//            requestPermissionCallbacks.add(it)
-//            requestPermission(it, Manifest.permission.CAMERA)
-//        }
-
         requestCustomPermissions(
             arrayOf(
                 Manifest.permission.CAMERA,
@@ -71,21 +72,6 @@ class MainActivity : AppCompatActivity() {
             )
         )
     }
-
-    private fun getPermissionResultCallback(onGranted: () -> Unit = {}) =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-//                    intiializeAndJoinChannel()
-                onGranted()
-                Toast
-                    .makeText(this, "Permission Granted", Toast.LENGTH_SHORT)
-                    .show()
-            } else {
-                Toast
-                    .makeText(this, "Permission Denied", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
 
     private fun intiializeAndJoinChannel() {
         try {
@@ -109,24 +95,17 @@ class MainActivity : AppCompatActivity() {
         rtcEngine?.joinChannel(token, channelName, "", 0)
     }
 
-    private fun requestPermission(
-        requestPermissionCallback: ActivityResultLauncher<String>,
-        permission: String
-    ) {
-        when {
-            ContextCompat.checkSelfPermission(
-                this,
-                permission
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                // TODO: ignore
-            }
-            shouldShowRequestPermissionRationale(permission) -> {
-                // TODO: Custom justification for the permission
-            }
-            else -> {
-                requestPermissionCallback.launch(permission)
-            }
-        }
+    private fun setUpRemoteUserVideoView(uid: Int) {
+        val remoteUserVideoContainer = findViewById<FrameLayout>(R.id.remoteUserContainer)
+        val remoteUserSurfaceView = RtcEngine.CreateRendererView(baseContext)
+        remoteUserVideoContainer.addView(remoteUserSurfaceView)
+        rtcEngine?.setupRemoteVideo(
+            VideoCanvas(
+                remoteUserSurfaceView,
+                VideoCanvas.RENDER_MODE_FIT,
+                uid
+            )
+        )
     }
 
     private fun requestCustomPermissions(permissions: Array<String>) {
@@ -139,7 +118,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        requestPermissions(permissionsRequired.toTypedArray(), APP_PERMISSION_REQUEST)
+        if (permissionsRequired.isNotEmpty()) {
+            requestPermissions(permissionsRequired.toTypedArray(), APP_PERMISSION_REQUEST)
+        } else {
+            intiializeAndJoinChannel()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == APP_PERMISSION_REQUEST && grantResults.isNotEmpty()) {
+            var isGranted = true
+            for (grantIndex in grantResults.indices) {
+                if (grantResults[grantIndex] == PackageManager.PERMISSION_GRANTED) {
+                    isGranted = true
+                } else {
+                    isGranted = false
+                    break
+                }
+            }
+
+            if (isGranted) {
+                intiializeAndJoinChannel()
+            }
+        }
     }
 
     override fun onDestroy() {
